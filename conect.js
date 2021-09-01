db42f1f59cee3739c06b03bf436032ef88644dba
Law5353 <89876934+Law5353@users.noreply.github.com>
2021-08-31 21:43:14 -0300

    Update and rename conect. to conect.js

--- conect.js
+++ conect.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
-
 const uri = "mongodb+srv://aluno:aluno@cluster0.tmvwi.mongodb.net/anime";
-
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set("useFindAndModify", false);
-
 module.exports = mongoose;
