3a2edc6870e40f7bc3ed2f9fbe9777b39e8e221f
Law5353 <89876934+Law5353@users.noreply.github.com>
2021-08-31 21:44:05 -0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,10 +1,8 @@
 var conexao = require("./conexao");
-
 var UsuarioScheme = conexao.Schema({
-  resul1: { type: int },
-  resul2: { type: int },
-  resul3: { type: int },
-  resul4: { type: int },
+resul1: { type: int },
+resul2: { type: int },
+resul3: { type: int },
+resul4: { type: int },
 });
-
 module.exports = conexao.model("Usuario", UsuarioScheme);
